b2e0fdb3e0dd9748a56adaa278480480bdceac4c
Josh <joshuakibwage23@gmail.com>
2025-01-16 15:18:19 +0300

    Add components

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReview from "./CustomerReview"
+import Footer from "./Footer"
+import Hero from "./Hero"
+import Services from "./Services"
+import SpecialOffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+import PopularProducts from "./PopularProducts"
+
+export {
+    CustomerReview,
+    Footer,
+    Hero,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts
+}
