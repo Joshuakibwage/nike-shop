6c3e0eaf4cae481d24e3f27c777162c6cc5fd358
Josh <joshuakibwage23@gmail.com>
2025-01-17 11:28:40 +0300

    Add super quality component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,14 @@
 const Button = ({label, iconURL}) => {
   return (
     <div>
-        <button className="flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white font-semibold border-coral-red">
+        <button 
+        className="flex items-center justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white font-semibold border-coral-red">
             {label}
-            <img 
+            {iconURL && <img 
                 src={iconURL} 
                 alt="arrow right icon" 
                 className="ml-2 rounded-full w-5 h-5"
-            />
+            />}
         </button>
     </div>
   )
