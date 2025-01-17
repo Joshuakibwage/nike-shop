33617a673cee5aee32539e0154dda64ca4bbe203
Josh <joshuakibwage23@gmail.com>
2025-01-17 10:54:17 +0300

    Add popular products component

---
+++ src/components/PopupalrProductCard.jsx.jsx
@@ -0,0 +1,28 @@
+import { star } from "../assets/icons"
+
+const PopularProductCard = ({imgURL, name, price}) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full group">
+      <img
+       src={imgURL} 
+       alt="product images"
+       className="w-[280px] h-[280px] group-hover:scale-100 transition-transform delay-200" 
+       />
+
+       <div className="flex mt-8 justify-start gap-2.5 ">
+        <img
+         src={star}
+         alt="rating" 
+         className=""
+         width={24}
+         height={24}
+         />
+         <p className="font-montserrat leading-normal text-xl text-slate-gray">(4.5)</p>
+       </div>
+       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
+       <p className="leading-normal font-montserrat text-2xl text-coral-red mt-2 font-semibold">{price}</p>
+    </div>
+  )
+}
+
+export default PopularProductCard
